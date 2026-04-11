d04d11409fce7cabff086f2cc0fec38935d60337
Mahima Raghuwanshi <hgraghuwanshi86@gmail.com>
2026-04-11 10:29:50 +0530

    average of marks

--- first.js
+++ first.js
@@ -1,8 +1,9 @@
-
-let names = ['raju', 'mahima','dabbu', 'ron']
-for  (let name of names )
-{console.log(name.toUpperCase());}
-
+//q)for a given array with marks of the students [85,97,44 , 37, 76, 60] find average marks of entire class
+let marks = [85,97,44,37, 76, 60] 
+let sum=0
+for (let mark=0;mark<marks.length ; mark++)
+{sum= sum+marks[mark]}
+console.log(sum/marks.length)
 
 
 
