bdc4618afae3326cfa965b66fe0d94579fec59a2
Mahima Raghuwanshi <hgraghuwanshi86@gmail.com>
2026-04-16 20:26:16 +0530

    methods

--- first.js
+++ first.js
@@ -1,16 +1,10 @@
-//q)for a given array with marks of the students [85,97,44 , 37, 76, 60] find average marks of entire class
-/*let marks = [85,97,44,37, 76, 60] 
-let sum=0
-for (let mark=0;mark<marks.length ; mark++)
-{sum= sum+marks[mark]}
-console.log(sum/marks.length)*/
-// next question) - for a given array with prices of 5 items [250,645,300,900,50] all items have an offer of 10% off on them . c hange the array to store final price after applying offer.
-let prices = [250,645,300,900,50]
-for(let price=0;price<prices.length;price++)
-{let discount = prices[price]*10/100;
-    prices[price]=prices[price]-discount;
-}
-console.log(prices)
+//methods of array 
+// to add 45 roll num at the end 
+let num =[89,90,87]
+num.push (45);
+console.log(num);
+
+
     
 
 
