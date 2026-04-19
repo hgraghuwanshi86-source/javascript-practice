5891bf420d87b047a798ebe2abc3759ac65b83c8
Mahima Raghuwanshi <hgraghuwanshi86@gmail.com>
2026-04-19 18:04:59 +0530

    functions

--- first.js
+++ first.js
@@ -24,6 +24,92 @@ let result = sum(3,4);
 console.log(result );
 
 //ARROW FUNCTION
+// sum of two numbers 
+const add = (a,b) => 
+   {return a+b; }
+console.log(add(12,80));
+
+//more shorter way (works for single expression)[implicit return]
+const square = num => num*num ;
+console.log(square(4));
+
+/*q) create a function using the "function" keyword that 
+     takes a string as an argument and returns the number
+     of vowels in the string?*/
+   
+// code :
+ 
+function countvowels (string) { let count=0
+
+   for (let char of string)
+      {if (char==='a' ||
+           char==='e'||
+           char==='i' ||
+           char==='o' ||
+           char==='u' )
+      {count++;}}
+      return count;
+}
+console.log (countvowels('icecream'));
+
+/*q) create a function using the "function" keyword that 
+     takes a string as an argument and returns the number
+     of vowels in the string?
+     write arrow function for thuis question !!*/
+
+  //code :
+  
+  const vowels = (string) => {let count=0
+
+   for (let char of string)
+      {if (char==='a' ||
+           char==='e'||
+           char==='i' ||
+           char==='o' ||
+           char==='u' )
+      {count++;}}
+      return count; }
+      console.log (vowels('skydiving'))
+;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
