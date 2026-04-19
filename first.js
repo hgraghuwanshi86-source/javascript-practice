39b6511caf23fb3c8aaed19c8814cc9c89dc1482
Mahima Raghuwanshi <hgraghuwanshi86@gmail.com>
2026-04-19 17:11:36 +0530

    functions topic

--- first.js
+++ first.js
@@ -1,31 +1,7 @@
-//methods of array 
-// to add 45 roll num at the end 
-/*let num =[89,90,87]
-num.push (45);
-console.log(num);*/
-//to delete the value and also return it 
-/*let state = ['bangladesh','punjab','maharashtra','goa']
-   let removed  = state.pop('goa')
-console.log (state);
-console.log (removed )*/
-
-//let state = ['bangladesh','punjab','maharashtra','goa']
-  // state.shift()
-//console.log (state);
-let state = ['bangladesh','punjab','maharashtra','goa']
-    state.unshift('gujarat')
-console.log (state);
-
-
-
-
-
-   
-
-
-
-    
-
+//creating and calling functions 
+function number (a)
+{console.log (a)};
+number(3);
 
 
 
