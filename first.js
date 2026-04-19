8521046a286444279823c700e95a15f3c5e231ff
Mahima Raghuwanshi <hgraghuwanshi86@gmail.com>
2026-04-19 17:39:55 +0530

    functions

--- first.js
+++ first.js
@@ -3,6 +3,31 @@ function number (a)
 {console.log (a)};
 number(3);
 
+//greet hello
+function greet()
+{console.log('hello');}
+greet();
+
+//parameters=input
+function greeting (name)
+{console.log('hello', name)};
+greeting("mahima");
+//not passed the parameter it shows undefined
+greeting();
+
+
+//(return)add two numbers 
+function sum (a,b) {
+  return a+b ;
+}
+let result = sum(3,4);
+console.log(result );
+
+//ARROW FUNCTION
+
+
+
+
 
 
  
